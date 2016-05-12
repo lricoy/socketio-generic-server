35d9d368045920fde5ca74fe087a4da6f1228580
Lucas Ricoy <ricoy.lucas@gmail.com>
2016-05-12 20:03:07 -0300

    feat(rooms): Option to emit only to a specific room
    If a 'join_room' event is emmited, the socket will join a specific room. After that if subsquent
    messages have a 'room' attr on it's first arg, they will only be emmited to that specific room

--- app.js
+++ app.js
@@ -2,17 +2,29 @@
 
 const PORT = process.env.PORT || 8000;
 
-let io = require('socket.io')(),
+var io = require('socket.io')(),
     middleware = require('socketio-wildcard')();
 
 io.use(middleware);
 
 io.on('connection', function(socket) {
+    socket.on('join_room', function (roomToJoin) {
+        socket.join(roomToJoin);
+    });
+
     socket.on('*', function(originalArgs) {
         const eventName = originalArgs.data.slice(0, 1)[0],
             args = originalArgs.data.slice(1, originalArgs.data.length);
 
-        io.emit(eventName, args);
+        if (eventName === 'join_room') return;
+
+        // Inspect the first message for a 'room' attr
+        const room = (args[0] || {}).room;    
+        
+        if (room)
+            io.to(room).emit(eventName, args);
+        else
+            io.emit(eventName, args);
     });
 });
 
--- demo/client.js
+++ demo/client.js
@@ -1,7 +1,8 @@
 var io = require('socket.io-client');
 var socket = io('http://localhost:8000');
 
-socket.emit('foo', 'bar');
+socket.emit('join_room', 'foobar');
+socket.emit('foo', {bar: 'bar', room: 'foobar'});
 socket.on('foo', function(args) {
     console.log(args); // -> ['bar']
 });
